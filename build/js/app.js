87fa1324419000aaaed7dffef3fd2f6614d6eabe
Stephanie Gurung and Terri Bradley <stephanie@>
2016-08-01 14:54:54 -0700

    attach node modules to run program through node

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Entry(title, body) {
   this.title = title;
   this.body = body;
@@ -51,3 +52,21 @@ Entry.prototype.teaser = function() {
   return teaser_array.join(' ');
 };
 exports.entryModule = Entry;
+
+},{}],2:[function(require,module,exports){
+var Entry = require("./../js/entry.js").entryModule;
+
+$(document).ready(function(){
+  $(".journal-entry").submit(function(event){
+  event.preventDefault();
+  var title = $("#title").val();
+  var body = $("#body").val();
+  var newEntry =new Entry(title, body);
+  $(".journal-entry").hide();
+  $(".description").append("<p>Title: " + newEntry.title + "</p><p>Entry: "+ newEntry.body + "</p>");
+  $(".counters").append("<p>There are this many words: " + newEntry.wordCounter() + "</p><p>There are this many vowels: " + newEntry.vowelCounter() + "</p><p>There are this many consonants: " + newEntry.consonantCounter() + "</p>");
+  $(".teaser").append(newEntry.teaser());
+  });
+});
+
+},{"./../js/entry.js":1}]},{},[2]);
